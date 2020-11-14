887d3596c138d71b499674da85a5bb03e1461303
El Idrissi Anass <anass.elidrissi@supelec.fr>
2020-11-14 02:14:53 +0100

    Resolve "Add unit testing for the collector"

--- scripts/dockerbuild.sh
+++ scripts/dockerbuild.sh
@@ -1,15 +1,18 @@
-apk update && apk add curl
+apk update && apk upgrade && apk add curl bash git openssh
 
 if [ $(curl --silent -f -lSL https://hub.docker.com/v2/repositories/$CI_REGISTRY_USER/$CI_REGISTRY_IMAGE/tags/$CI_COMMIT_BRANCH 2>/dev/null) ]; then
-    docker pull $CI_REGISTRY_USER/$CI_REGISTRY_IMAGE:$CI_COMMIT_BRANCH
+    IMAGE_BRANCH=$CI_COMMIT_BRANCH
+else
+    IMAGE_BRANCH=$CI_DEFAULT_BRANCH
 fi 
 
-docker pull $CI_REGISTRY_USER/$CI_REGISTRY_IMAGE:$CI_DEFAULT_BRANCH
+docker pull $CI_REGISTRY_USER/$CI_REGISTRY_IMAGE:$IMAGE_BRANCH
+git clone --depth 1 --no-single-branch https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab-student.centralesupelec.fr:tweetoscope-mullticolore/tweetou.git
 
-if [ $(git diff master --name-only | grep -e requirements.txt -e .gitlab-ci.yml) ]; then
-    docker build -t $CI_REGISTRY_USER/$CI_REGISTRY_IMAGE:$CI_COMMIT_BRANCH --cache-from $CI_REGISTRY_USER/$CI_REGISTRY_IMAGE:$CI_COMMIT_BRANCH --cache-from $CI_REGISTRY_USER/$CI_REGISTRY_IMAGE:$CI_DEFAULT_BRANCH . > docker-build.log
+if [ $(git --git-dir tweetou/.git diff origin/$CI_DEFAULT_BRANCH origin/CI_COMMIT_BRANCH --name-only | grep -e Dockerfile -e requirements.txt -e .gitlab-ci.yml) ]; then
+    docker build -t $CI_REGISTRY_USER/$CI_REGISTRY_IMAGE:$CI_COMMIT_BRANCH --cache-from $CI_REGISTRY_USER/$CI_REGISTRY_IMAGE:$IMAGE_BRANCH . > docker-build.log
 else
-    docker tag $CI_REGISTRY_USER/$CI_REGISTRY_IMAGE:$CI_DEFAULT_BRANCH $CI_REGISTRY_USER/$CI_REGISTRY_IMAGE:$CI_COMMIT_BRANCH 
+    docker tag $CI_REGISTRY_USER/$CI_REGISTRY_IMAGE:$IMAGE_BRANCH $CI_REGISTRY_USER/$CI_REGISTRY_IMAGE:$CI_COMMIT_BRANCH 
 fi
 
 docker push $CI_REGISTRY_USER/$CI_REGISTRY_IMAGE:$CI_COMMIT_BRANCH
--- src/cpp/TweetCollector/main.cpp
+++ src/cpp/TweetCollector/main.cpp
@@ -1,9 +1,8 @@
-#include "TweetCollectorParams.hpp"
-#include "TweetStreams.hpp"
+#include <iostream>
+#include "../Consumer.hpp"
+#include "../Producer.hpp"
 #include "Processor.hpp"
-
-#include <cppkafka/configuration.h>
-#include <cppkafka/cppkafka.h>
+#include "TweetCollectorParams.hpp"
 
 int main(int argc, char* argv[])
 {
@@ -22,29 +21,14 @@ int main(int argc, char* argv[])
         std::cout << "Using default config path: config/collector.ini" <<std::endl;
         param_path = "config/collector.ini";
     }
-    tweetoscope::params::collector params(param_path);
 
-    cppkafka::Configuration config = {
-        { "bootstrap.servers", params.kafka.brokers},
-        { "auto.offset.reset", params.kafka.offset_reset},
-        { "group.id", params.kafka.group}
-    };
-
-    std::cout << std::endl
-        << "Parameters : " << std::endl
-        << "----------"    << std::endl
-        << std::endl
-        << params << std::endl
-        << std::endl;
-    
-    cppkafka::Consumer consumer(config); 
-    processor::MessageHandler handler(params.times.terminated, params.times.observation);
-
-    consumer.subscribe({params.topic.in});
+    const tweetoscope::params::collector params(param_path);
+    kafka::TweetConsumer consumer(params);
+    processor::MessageHandler handler(params);
 
     while(true)
     {
-        auto msg = consumer.poll();
+        auto msg = consumer.get_message();
         if( msg && ! msg.get_error() )
         {
             handler(std::move(msg));
